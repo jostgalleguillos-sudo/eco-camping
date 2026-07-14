f200d635018ad38da4e49e918e08f32b5ab770d7
jostgalleguillos-sudo <jost.galleguillos@duocuc.cl>
2026-07-13 21:21:35 -0400

    Create bodega.py

---
+++ bodega.py
@@ -0,0 +1,25 @@
+arreglos = {
+    'FLO1': ['Ramo Primavera', 'ramo', 'rosado','M', True, 'primavera'],
+    'FLO2': ['Caja Elegante', 'caja', 'blanco', 'L', True, 'todo año'],
+    'FLO3': ['Ramo Solar', 'ramo', 'amarillo', 'S', False, 'verano'],
+    'FLO4': ['Centro Mesa', 'centro', 'rojo', 'M', True, 'todo año'],
+    'FLO5': ['Ramo Bosque', 'ramo', 'verde', 'L', False, 'otoño'],
+    'FLO6': ['Caja Noche', 'caja', 'morado', 'M', True, 'invierno'],
+}
+bodega = {
+    'FLO1': [15990, 8],
+    'FLO2': [29990, 3],
+    'FLO3': [990, 12],
+    'FLO4': [24990, 5],
+    'FLO5': [19990, 0],
+    'FLO6': [229990, 6],
+
+}
+busqueda_precio(p_min, p_max) = {}
+def actualizar_precio(codigo,nuevo_precio)
+buscar_codigo = codigo[codigo]
+                print(f"codigo encontrado: {posicion}")
+                print(f"Nombre: {serv['nombre']}")
+                print(f"Uptime: {serv['uptime']} días")
+                print(f"Carga: {serv['carga']}")
+                print(f"Estado Critico: {'SI' if serv['critico'] else 'NO'}")
